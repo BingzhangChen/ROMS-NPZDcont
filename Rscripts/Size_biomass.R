9c0015a8f005c92b48511982d1ccfc71f25122cc
Bingzhang Chen <bingzhang.chen@gmail.com>
2017-05-29 14:46:22 +0900

    First commit for adding the R script for plotting total biomass and size fractions

---
+++ Rscripts/Size_biomass.R
@@ -0,0 +1,8 @@
+#Extract mean size, size variance, and the relationships with total biomass from surface
+nameit = 'npacific'
+pwd1   = 'npacific'
+setwd(paste0('~/Roms_tools/Run/',pwd1))
+
+#Load total biomass:
+
+
