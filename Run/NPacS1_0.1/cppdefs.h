284ad3b5fda55fb28fb455938a396120678cc29d
Default Seadas User <seadas-user@localhost>
2018-10-24 12:49:34 +0900

    Add bio_MOD.F and *.h to NPacS1_0.1

---
+++ Run/NPacS1_0.1/cppdefs.h
@@ -0,0 +1,871 @@
+/*
+   This is "cppdefs.h": MODEL CONFIGURATION FILE
+   ==== == ============ ===== ============= ====
+*/
+#define REGIONAL        /* REGIONAL Applications */
+#if defined REGIONAL
+/*
+!====================================================================
+!               REGIONAL (realistic) Configurations
+!==================================================================== 
+!
+!----------------------
+! BASIC OPTIONS
+!----------------------
+!
+*/
+                      /* Configuration Name */
+# define NPACS
+                      /* Parallelization */
+# undef OPENMP
+# define MPI
+                      /* Nesting */
+# undef  AGRIF
+# undef  AGRIF_2WAY
+                      /* OA Coupling via OASIS (MPI) */
+# undef  OA_COUPLING
+                      /* Open Boundary Conditions */
+# undef  TIDES
+# undef OBC_EAST
+# undef OBC_WEST
+# undef OBC_NORTH
+# define OBC_SOUTH
+                      /* Applications */
+# define  BIOLOGY
+# undef  FLOATS
+# undef  STATIONS
+# undef  PASSIVE_TRACER
+# undef  SEDIMENT
+# undef  BBL
+/*!
+!-------------------------------------------------
+! PRE-SELECTED OPTIONS
+!
+! ADVANCED OPTIONS ARE IN SET_GLOBAL_DEFINITIONS.H
+!-------------------------------------------------
+*/
+                      /* Parallelization */
+# ifdef MPI
+#  undef  PARALLEL_FILES
+# endif
+# undef  AUTOTILING
+# undef  ETALON_CHECK
+                      /* Grid configuration */
+# define CURVGRID
+# define SPHERICAL
+# define MASKING
+# undef  WET_DRY
+# define NEW_S_COORD
+                      /* Model dynamics */
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+                      /* Equation of State */
+# define SALINITY
+# define NONLIN_EOS
+# define SPLIT_EOS
+                      /* Lateral Explicit Momentum Mixing */
+# define UV_VIS2
+# ifdef UV_VIS2
+#  define UV_MIX_S
+#  define UV_VIS_SMAGO
+# endif
+                      /* Lateral Tracer Advection (default UP3) */
+# define TS_HADV_RSUP3
+# undef  TS_HADV_UP5
+# undef  TS_HADV_C4
+# undef  TS_HADV_WENO5
+                      /* Lateral Explicit Tracer Mixing */
+# undef TS_MIX_S
+# ifdef TS_HADV_C4
+#  define  TS_DIF2
+#  undef   TS_DIF4
+#  define  TS_DIF_SMAGO
+#  define  TS_MIX_ISO
+# endif
+                      /* Sponge layers for UV and TS */
+# define SPONGE
+                      /* Vertical Mixing */
+# undef  BODYFORCE
+# undef  BVF_MIXING
+# define LMD_MIXING
+# undef  GLS_MIXING
+# ifdef LMD_MIXING
+#  define LMD_SKPP
+#  define LMD_BKPP
+#  define LMD_RIMIX
+#  define LMD_CONVEC
+#  define LMD_DDMIX
+#  define LMD_NONLOCAL
+# endif
+# ifdef GLS_MIXING
+#  define GLS_KKL
+#  undef  GLS_KOMEGA
+#  undef  GLS_KEPSILON
+#  undef  GLS_GEN
+#  undef  KANTHA_CLAYSON
+#  undef  CRAIG_BANNER
+#  undef  CANUTO_A
+#  undef  ZOS_HSIG
+# endif
+                      /* Surface Forcing */
+# define  BULK_FLUX
+# ifdef BULK_FLUX
+#  define BULK_FAIRALL
+#  define BULK_LW      // Downward long wavelength radiation
+#  define BULK_EP      // Activate the bulk parametrization for salinity fluxes
+#  define BULK_SMFLUX
+#  define BULK_SM_UPDATE
+#  define  SST_SKIN
+#  undef  ANA_DIURNAL_SW
+#  undef  ONLINE
+#  define  QCORRECTION
+#  define  SFLX_CORR
+# else
+#  define QCORRECTION
+#  define SFLX_CORR
+#  undef ANA_DIURNAL_SW
+# endif
+                      /* Lateral Forcing */
+# undef CLIMATOLOGY
+# ifdef CLIMATOLOGY
+#  undef ZCLIMATOLOGY
+#  undef M2CLIMATOLOGY
+#  undef M3CLIMATOLOGY
+#  undef TCLIMATOLOGY
+#  undef ZNUDGING
+#  undef M2NUDGING
+#  undef M3NUDGING
+#  undef TNUDGING
+#  undef ROBUST_DIAG
+# endif
+
+# define  FRC_BRY
+# ifdef FRC_BRY
+#  define Z_FRC_BRY
+#  define M2_FRC_BRY
+#  define M3_FRC_BRY
+#  define T_FRC_BRY
+# endif
+# undef EW_PERIODIC
+                      /* Bottom Forcing */
+# define ANA_BSFLUX
+# define ANA_BTFLUX
+                      /* Point Sources - Rivers */
+# undef PSOURCE
+# undef PSOURCE_NCFILE
+# ifdef PSOURCE_NCFILE                    
+#   define PSOURCE_NCFILE_TS
+# endif
+                      /* Open Boundary Conditions */
+# ifdef TIDES
+#  define SSH_TIDES
+#  define UV_TIDES
+#  define TIDERAMP
+#  define OBC_M2FLATHER
+# else
+#  undef  OBC_M2SPECIFIED
+#  undef  OBC_M2FLATHER
+#  define  OBC_M2CHARACT
+#  undef  OBC_M2ORLANSKI
+#  ifdef  OBC_M2ORLANSKI
+#   define OBC_VOLCONS
+#  endif
+# endif
+# define OBC_M3ORLANSKI
+# define OBC_TORLANSKI
+# undef  OBC_M3SPECIFIED
+# undef  OBC_TSPECIFIED
+                      /* Input/Output & Diagnostics */
+# define AVERAGES
+# define AVERAGES_K
+# undef  DIAGNOSTICS_TS
+# undef  DIAGNOSTICS_UV
+# ifdef DIAGNOSTICS_TS
+#  undef DIAGNOSTICS_TS_ADV
+#  undef DIAGNOSTICS_TS_MLD
+# endif
+/*
+!           Applications:
+!---------------------------------
+! Biology, floats, Stations, 
+! Passive tracer, Sediments, BBL
+!---------------------------------
+!
+   Quasi-monotone lateral advection scheme (WENO5)
+   for passive/biology/sediment tracers 
+*/
+# if defined PASSIVE_TRACER || defined BIOLOGY || defined SEDIMENT
+#  undef BIO_HADV_WENO5
+# endif
+                      /*   Choice of Biology models   */
+# ifdef BIOLOGY
+#  undef PISCES
+#  undef BIO_NChlPZD
+#  undef BIO_N2ChlPZD2
+#  undef BIO_BioEBUS
+#  undef BIO_FLEX
+#  undef BIO_PZEFT
+#  define PZEFTFE 
+#   ifdef PZEFTFE
+#     undef CITRATE3
+#   endif
+#  undef BIO_NPZDDISC
+                      /*   Biology options    */
+#  ifdef PISCES
+#   define DIURNAL_INPUT_SRFLX
+#   define key_trc_pisces
+#   define key_passivetrc
+#  endif
+#  ifdef BIO_NChlPZD
+#   undef  OXYGEN
+#  endif
+#  ifdef BIO_BioEBUS
+#   define NITROUS_OXIDE
+#  endif
+#  if defined BIO_PZEFT || defined PZEFTFE
+#   undef NPZDSIMPLE
+#   undef   EFTNOSIZE
+#   undef   NPZDSIZE
+#   undef  EFTSIZE
+#   define IRON
+#  endif
+                      /*   Biology diagnostics    */
+#  undef DEBUG_BIO2 
+#  define DIAGNOSTICS_BIO
+#  if defined DIAGNOSTICS_BIO && defined PISCES
+#   define key_trc_diaadd
+#   define key_trc_dia3d
+#  endif
+# endif
+                      /*   Lagrangian floats model    */
+# ifdef FLOATS
+#  undef  FLOATS_GLOBAL_ATTRIBUTES
+#  undef  IBM
+#  undef  RANDOM_WALK
+#  ifdef RANDOM_WALK
+#   define DIEL_MIGRATION
+#   define RANDOM_VERTICAL
+#   define RANDOM_HORIZONTAL
+#  endif
+# endif
+                      /*   Stations recording    */
+# ifdef STATIONS
+#  define ALL_SIGMA
+# endif
+                      /*   Sediment dynamics model     */
+# ifdef SEDIMENT
+#  define ANA_SEDIMENT
+#  undef  BED_ARMOR
+#  undef  ANA_SPFLUX
+#  undef  ANA_BPFLUX
+# endif
+                      /*   Bottom Boundary Layer model     */
+# ifdef BBL
+#  define ANA_WWAVE
+# endif
+/*
+!
+!==========================================================
+!              IDEALIZED CONFIGURATIONS
+!==========================================================
+!
+*/
+#elif defined BASIN
+/*
+!                       Basin Example
+!                       ===== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define UV_VIS2
+# define UV_MIX_S
+# define SOLVE3D
+# define TS_DIF2
+# define TS_MIX_S
+# define BODYFORCE
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_BTFLUX
+
+#elif defined CANYON_A
+/*
+!                       First Canyon Example
+!                       ===== ====== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define SOLVE3D
+# define EW_PERIODIC
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_BTFLUX
+
+#elif defined CANYON_B
+/*
+!                       Second Canyon Example
+!                       ====== ====== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define SOLVE3D
+# define EW_PERIODIC
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_BTFLUX
+# define ANA_VMIX
+
+#elif defined EQUATOR
+/*
+!                       Equator Example
+!                       ======= =======
+! Boccaletti, G., R.C. Pacanowski, G.H. Philander and A.V. Fedorov, 2004,
+! The Thermal Structure of the Upper Ocean, J.Phys.Oceanogr., 34, 888-902.
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define UV_VIS2
+# define UV_MIX_S
+# define SOLVE3D
+# define SALINITY
+# define TS_DIF2
+# define TS_MIX_S
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SRFLUX
+# define ANA_SSFLUX
+# define ANA_BTFLUX
+# define ANA_BSFLUX
+# define QCORRECTION
+# define ANA_SST
+# define LMD_SKPP /* problem with MPI in Xi direction */
+# define LMD_MIXING
+# define LMD_RIMIX
+# define LMD_CONVEC
+
+#elif defined GRAV_ADJ
+/*
+!                       Gravitational Adjustment Example
+!                       ============= ========== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define SOLVE3D
+# define UV_ADV
+# define UV_VIS2
+# define UV_MIX_S
+# undef  TS_HADV_WENO5
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_BTFLUX
+
+#elif defined INNERSHELF
+/*
+!                       Inner Shelf Example
+!                       ===== ===== =======
+*/
+# define ETALON_CHECK
+# undef  OPENMP
+# undef  MPI
+# define INNERSHELF_EKMAN
+# define INNERSHELF_APG
+# define SOLVE3D
+# define UV_COR
+# define ANA_GRID
+# define ANA_INITIAL
+# define AVERAGES
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_STFLUX
+# define ANA_BSFLUX
+# define ANA_BTFLUX
+# define ANA_SMFLUX
+# define NS_PERIODIC
+# define OBC_WEST
+# define OBC_M2ORLANSKI
+# define OBC_VOLCONS
+# define OBC_TORLANSKI
+# define OBC_M3ORLANSKI
+# define SPONGE
+# ifndef INNERSHELF_EKMAN
+#  define UV_ADV
+#  define SALINITY
+#  define NONLIN_EOS
+#  define SPLIT_EOS
+#  define LMD_MIXING
+#  ifdef LMD_MIXING
+#   define LMD_SKPP
+#   define LMD_BKPP
+#   define LMD_RIMIX
+#   define LMD_CONVEC
+#  else
+#   define GLS_MIXING
+#   define GLS_KKL
+#  endif
+# endif
+
+#elif defined INTERNAL
+/*
+!                       Internal Tide Example
+!                       ======== ==== =======
+!
+! Di Lorenzo, E, W.R. Young and S.L. Smith, 2006, Numerical and anlytical estimates of M2
+! tidal conversion at steep oceanic ridges, J. Phys. Oceanogr., 36, 1072-1084.  
+*/
+# undef  ETALON_CHECK
+# undef  OPENMP
+# undef  MPI
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+# define BODYTIDE
+# define ANA_GRID
+# undef  INTERNALSHELF
+# define ANA_INITIAL
+# define ANA_BTFLUX
+# define ANA_SMFLUX
+# define ANA_SRFLUX
+# define ANA_STFLUX
+# define ANA_VMIX
+
+# undef EW_PERIODIC
+# undef NS_PERIODIC
+
+# undef  UV_VIS2
+# undef  UV_MIX_GEO
+# undef  TS_DIF2
+# undef  TS_MIX_GEO
+# undef  SPONGE
+# undef  ANA_SSH
+# undef  ANA_M2CLIMA
+# undef  ANA_M3CLIMA
+# undef  ANA_TCLIMA
+# undef  ZCLIMATOLOGY
+# undef  M2CLIMATOLOGY
+# undef  M3CLIMATOLOGY
+# undef  TCLIMATOLOGY
+# undef  ZNUDGING
+# undef  M2NUDGING
+# undef  M3NUDGING
+# undef  TNUDGING
+# undef  OBC_EAST
+# undef  OBC_WEST
+# undef  OBC_M2CHARACT
+# undef  OBC_M2FLATHER
+# undef  OBC_TORLANSKI
+# undef  OBC_M3ORLANSKI
+
+#elif defined RIVER
+/*
+!                       River run-off test problem
+!                       ==========================
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define SOLVE3D
+# define UV_ADV
+# define UV_COR
+# define M2FILTER_FLAT
+# define NONLIN_EOS
+# define SPLIT_EOS
+# define SALINITY
+# define ANA_GRID
+# define MASKING
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_BTFLUX
+# define ANA_BSFLUX
+# define LMD_MIXING
+# define LMD_SKPP
+# define LMD_BKPP
+# define LMD_RIMIX
+# define LMD_CONVEC
+# define PSOURCE
+# define ANA_PSOURCE
+# define NS_PERIODIC
+# define FLOATS
+# ifdef FLOATS
+#   define RANDOM_WALK
+#   ifdef RANDOM_WALK
+#      define DIEL_MIGRATION
+#      define RANDOM_VERTICAL
+#      define RANDOM_HORIZONTAL
+#   endif
+# endif
+
+#elif defined SEAMOUNT
+/*
+!                       Seamount Example
+!                       ======== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define SOLVE3D
+# define SALINITY
+# define NONLIN_EOS
+# define SPLIT_EOS
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_BTFLUX
+# define ANA_BSFLUX
+
+# elif defined SHELFRONT
+/*
+!                       Shelf Front Example
+!                       ===== ===== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define SOLVE3D
+# define SALINITY
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_BTFLUX
+# define ANA_BSFLUX
+# define EW_PERIODIC
+
+#elif defined SOLITON
+/*
+!                       Equatorial Rossby Wave Example
+!                       ========== ====== ==== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_COR
+# define UV_ADV
+# define ANA_GRID
+# define ANA_INITIAL
+# define AVERAGES
+# define EW_PERIODIC
+# define ANA_SMFLUX
+
+# elif defined OVERFLOW
+/*
+!                       Gravitational/Overflow Example
+!                       ====================== =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define UV_ADV
+# define UV_COR
+# define UV_VIS2
+# define UV_MIX_GEO
+# define TS_DIF2
+# define TS_MIX_GEO
+# define SOLVE3D
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_BTFLUX
+
+#elif defined UPWELLING
+/*
+!                       Upwelling Example
+!                       ========= =======
+*/
+# define ETALON_CHECK
+# undef OPENMP
+# undef MPI
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+# define ANA_GRID
+# define ANA_INITIAL
+# define AVERAGES
+# define SALINITY
+# define NONLIN_EOS
+# define SPLIT_EOS
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_STFLUX
+# define ANA_BSFLUX
+# define ANA_BTFLUX
+# define ANA_SMFLUX
+# define LMD_MIXING
+# define LMD_SKPP
+# define LMD_BKPP
+# define LMD_RIMIX
+# define LMD_CONVEC
+# define EW_PERIODIC
+
+#elif defined VORTEX
+/*
+!                       Baroclinic Vortex Example (TEST AGRIF)
+!                       ========== ====== ======= ===== ======
+*/
+# undef  ETALON_CHECK
+# undef  OPENMP
+# undef  MPI
+# define AGRIF
+# define AGRIF_2WAY
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+# define ANA_STFLUX
+# define ANA_SMFLUX
+# define ANA_BSFLUX
+# define ANA_BTFLUX
+# define ANA_VMIX
+# define SPONGE
+# define TS_MIX_S
+# define ZCLIMATOLOGY
+# define M2CLIMATOLOGY
+# define M3CLIMATOLOGY
+# define TCLIMATOLOGY
+# define ZNUDGING
+# define M2NUDGING
+# define M3NUDGING
+# define TNUDGING
+# define OBC_EAST
+# define OBC_WEST
+# define OBC_NORTH
+# define OBC_SOUTH
+# define OBC_M2FLATHER
+# define OBC_TORLANSKI
+# define OBC_M3ORLANSKI
+
+#elif defined JET
+/*
+!                       Baroclinic JET Example
+!                       ========== === =======
+*/
+# define ANA_JET
+# undef  ETALON_CHECK
+# undef  MPI
+# undef  AGRIF
+# undef  AGRIF_2WAY
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+# define UV_VIS2
+# define UV_MIX_S
+# ifdef ANA_JET
+#  define ANA_GRID
+#  define ANA_INITIAL
+# endif
+# define ANA_STFLUX
+# define ANA_SMFLUX
+# define ANA_BSFLUX
+# define ANA_BTFLUX
+# define ANA_VMIX
+# define EW_PERIODIC
+# define CLIMATOLOGY
+# ifdef CLIMATOLOGY
+#  define ZCLIMATOLOGY
+#  define M2CLIMATOLOGY
+#  define M3CLIMATOLOGY
+#  define TCLIMATOLOGY
+#  define ZNUDGING
+#  define M2NUDGING
+#  define M3NUDGING
+#  define TNUDGING
+#  define ROBUST_DIAG
+#  define ZONAL_NUDGING
+#  ifdef ANA_JET
+#   define ANA_SSH
+#   define ANA_M2CLIMA
+#   define ANA_M3CLIMA
+#   define ANA_TCLIMA
+#  endif
+# endif
+# define LMD_MIXING 
+# ifdef  LMD_MIXING
+#  undef  ANA_VMIX
+#  define ANA_SRFLUX
+#  undef  LMD_KPP
+#  define LMD_RIMIX
+#  define LMD_CONVEC
+# endif 
+
+#elif defined SHOREFACE
+/*
+!                       PLANAR BEACH Example
+!                       ====== ===== =======
+!
+!   Uchiyama, Y., McWilliams, J.C. and Shchepetkin, A.F. (2010): 
+!      Wave-current interaction in an oceanic circulation model with a 
+!      vortex force formalism: Application to the surf zone.
+!      Ocean Modelling Vol. 34:1-2, pp.16-35.
+*/
+# undef  ETALON_CHECK
+# undef  OPENMP
+# undef  MPI
+# define SOLVE3D
+# define UV_ADV
+# undef  MASKING
+# define NEW_S_COORD
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_SST
+# define ANA_BTFLUX
+# define NS_PERIODIC
+# define OBC_WEST
+# ifdef OBC_WEST
+#  define OBC_M2CHARACT
+#  define OBC_M3ORLANSKI
+#  define OBC_TORLANSKI
+#  define SPONGE
+#  define TS_MIX_S
+# endif
+# define WET_DRY
+# define MRL_WCI
+# ifdef MRL_WCI
+#  undef  WKB_WWAVE
+#  undef  WAVE_ROLLER
+#  undef  WAVE_STREAMING
+#  undef  MRL_CEW
+#  define WAVE_RAMP
+# endif
+# define LMD_MIXING
+# define LMD_SKPP
+# define LMD_BKPP
+# undef  BBL
+# undef  SEDIMENT
+
+#elif defined RIP
+/*
+!                       Rip Current Example
+!                       === ======= =======
+!
+!   Weir, B., Uchiyama, Y.. (2010): 
+!      A vortex force analysis of the interaction of rip 
+!      currents and surface gravity wave
+!      JGR Vol. 116
+*/
+# undef  BISCA
+# undef  DIAGNOSTICS_UV
+# undef  OPENMP
+# undef  MPI
+# define SOLVE3D
+# define UV_ADV
+# define NEW_S_COORD
+# ifndef BISCA
+#  define ANA_GRID
+# endif
+# define ANA_INITIAL
+# define ANA_SMFLUX
+# define ANA_STFLUX
+# define ANA_SSFLUX
+# define ANA_SRFLUX
+# define ANA_SST
+# define ANA_BTFLUX
+# ifndef BISCA
+#  define NS_PERIODIC
+# endif
+# define OBC_WEST
+# ifdef BISCA
+#  define OBC_NORTH
+#  define OBC_SOUTH
+# endif
+# ifdef OBC_WEST
+#  define OBC_M2CHARACT
+#  define OBC_M3ORLANSKI
+#  define OBC_TORLANSKI
+#  define SPONGE
+# endif
+# define WET_DRY
+# define MRL_WCI
+# ifdef MRL_WCI
+#  define WKB_WWAVE
+#  define WKB_OBC_WEST
+#  define WAVE_ROLLER
+#  define WAVE_STREAMING
+#  define WAVE_FRICTION
+#  define MRL_CEW
+#  undef  WKB_KZ_FILTER
+#  undef  WKB_TIME_FILTER
+#  undef  WAVE_RAMP
+# endif
+# define LMD_MIXING
+# define LMD_SKPP
+# define LMD_BKPP
+# define UV_VIS2
+# ifdef UV_VIS2
+#  define UV_MIX_S
+#  define UV_VIS_SMAGO
+# endif
+# define BBL
+# undef  SEDIMENT
+# ifdef SEDIMENT
+#  define ANA_SEDIMENT
+#  undef  BED_ARMOR
+#  undef  ANA_SPFLUX
+#  undef  ANA_BPFLUX
+# endif
+
+#elif defined THACKER
+/*
+!                       Thacker Example
+!                       ======= =======
+!
+! Thacker, W., (1981), Some exact solutions to the nonlinear shallow-water wave equations. 
+! J. Fluid Mech., 107, 499–508.
+*/
+# undef  ETALON_CHECK
+# undef  OPENMP
+# undef  MPI
+# define SOLVE3D
+# define UV_COR
+# define UV_ADV
+# define WET_DRY
+# define NEW_S_COORD
+# define ANA_GRID
+# define ANA_INITIAL
+# define ANA_BTFLUX
+# define ANA_SMFLUX
+# define ANA_SRFLUX
+# define ANA_STFLUX
+
+
+#endif /* END OF CONFIGURATION CHOICE */
+
+#include "set_global_definitions.h"
+
